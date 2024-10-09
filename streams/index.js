843fbc1b994dd27f22d6800ec2f2c3a19acb3fdf
Viktor <palchinskij19@gmail.com>
2024-10-09 14:31:17 +0300

    09.10.24 - transform stream

--- streams/index.js
+++ streams/index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const { Transform } = require('stream');
 
 const r = fs.createReadStream('./source.txt');
 const w = fs.createWriteStream('./destination.txt');
@@ -12,3 +13,12 @@ r.on('data', (c) => console.log(`Chunk of data was received: ${c} `));
 r.on('end', () => console.log('Reading finished'));
 
 r.on('error', (e) => console.log(`Error : ${e} `));
+
+const upperCaseTransform = new Transform({
+    transform(chunk, encoding, callback) {
+        this.push(chunk.toString().toUpperCase());
+        callback();
+    },
+});
+
+process.stdin.pipe(upperCaseTransform).pipe(process.stdout);
